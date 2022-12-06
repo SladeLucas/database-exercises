fc9440116ad145302ac55a672cc6da0d75878093
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-06 13:42:51 -0700

    added some things to a table

---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+    INSERT INTO albums (artist, name, release_date, sales, genre)
+    VALUES ('Michael Jackson', 'Thriller', 1982, 50, 'Pop'),
+           ('AC/DC', 'Back in Black', 1980, 30, 'Hard Rock'),
+           ('Eagles', 'Their Greatest Hits', 1976, 41, 'Country Rock'),
+           ('Eagles', 'Hotel California', 1976, 32, 'Soft Rock'),
+           ('Shania Twain', 'Come On Over', 1997, 30, 'Country'),
+           ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30, 'Hard Rock');
