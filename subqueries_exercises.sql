af969dcd7b4f241c8f4b5cd11a9414ffd6cf5d45
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-12 15:25:33 -0700

    found female managers

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,3 +6,11 @@ USE employees;
     SELECT DISTINCT title FROM titles
     WHERE emp_no IN
           (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+    SELECT first_name, last_name
+    FROM employees
+    WHERE emp_no IN
+          (SELECT emp_no FROM dept_manager WHERE gender = 'F' AND to_date > now());
+
+
+
