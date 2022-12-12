1977d8b35c8c341283ad94404437eaac84dad2ca
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-12 13:11:26 -0700

    findng some managers in the database

---
+++ join_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+    SELECT dept_name AS 'Department Name', concat(employees.first_name, ' ', employees.last_name) AS 'Department Manager'
+    FROM employees
+        JOIN dept_manager
+            ON employees.emp_no = dept_manager.emp_no
+        JOIN departments
+            ON dept_manager.dept_no = departments.dept_no
+    WHERE to_date LIKE '9999-%'
+    ORDER BY dept_name;
