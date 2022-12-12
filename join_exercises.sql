7d98cf3c257e1fb706f776f7df711d954fded155
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-12 13:19:22 -0700

    found the managers that happened to be also Female

--- join_exercises.sql
+++ join_exercises.sql
@@ -7,3 +7,13 @@ USE employees;
             ON dept_manager.dept_no = departments.dept_no
     WHERE to_date LIKE '9999-%'
     ORDER BY dept_name;
+
+    SELECT dept_name AS 'Department Name', CONCAT(employees.first_name, ' ', employees.last_name) AS 'Department Manager'
+    FROM employees
+        JOIN dept_manager
+            ON employees.emp_no = dept_manager.emp_no
+        JOIN departments
+            ON dept_manager.dept_no = departments.dept_no
+    WHERE to_date LIKE '9999-%' AND gender = 'F'
+    ORDER BY dept_name;
+
