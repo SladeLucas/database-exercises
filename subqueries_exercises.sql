01758967cfe1cff4350378bbcb92b55623e5d428
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-12 15:21:02 -0700

    found distinct titles by employee name

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,3 +2,7 @@ USE employees;
     SELECT * FROM employees
     WHERE hire_date IN
           (SELECT hire_date FROM employees WHERE emp_no = '101010');
+
+    SELECT DISTINCT title FROM titles
+    WHERE emp_no IN
+          (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
