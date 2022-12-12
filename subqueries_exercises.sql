a1b9af74c0ff37ef95f8fd21f4c50b6981b5797b
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-12-12 15:37:34 -0700

    found the highest paid actor

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -12,5 +12,13 @@ USE employees;
     WHERE emp_no IN
           (SELECT emp_no FROM dept_manager WHERE gender = 'F' AND to_date > now());
 
+    SELECT dept_name
+    FROM departments
+    WHERE dept_no IN
+          (SELECT dept_no FROM dept_manager WHERE to_date > now() AND emp_no IN
+                (SELECT emp_no FROM employees WHERE gender = 'F'));
 
-
+    SELECT first_name, last_name FROM employees
+    WHERE emp_no IN
+          (SELECT emp_no FROM salaries WHERE salary IN
+                (SELECT MAX(salary) FROM salaries));
